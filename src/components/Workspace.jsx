9ccef1aa6394e72fd8d2e873ee735e26997fc529
Rami <ramirim059@gmail.com>
2024-08-20 15:13:34 +0200

    code revert

--- src/components/Workspace.jsx
+++ src/components/Workspace.jsx
@@ -1,50 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, Link, Route, Routes, useLocation } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {useParams, Link, Route, Routes, useLocation} from 'react-router-dom';
 import './Workspace.css';
 import Boards from './Boards';
 import Navbar from "./Navbar/Navbar.jsx";
-import PropTypes from 'prop-types';
-import RoleService from '../Services/RoleService';
-import BoardService from '../Services/BoardService';
 
-const Workspace = ({ isVisible }) => {
+import PropTypes from 'prop-types';  // Import PropTypes
+
+import RoleService from '../Services/RoleService';  // Import RoleService to fetch roles
+import BoardService from '../Services/BoardService';
+// import ProjectService from "../Services/ProjectService.js";  // Adjust import path as necessary
+
+const Workspace = ({isVisible}) => {
     const [roles, setRoles] = useState([]);
     const [selected_roleId, setSelected_roleId] = useState(null);
-    const [projectId, setProjectId] = useState(null);
-    const [projectDescription, setProjectDescription] = useState(null);
+    const [projectId, setProjectId] = useState(null); // Assuming you have a way to set the projectId
+    const [projectDescription, setProjectDescription] = useState(null); // Assuming you have a way to set the projectId
+
+    const {projectName} = useParams();
     const [selectedBoard, setSelectedBoard] = useState(null);
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const [boards, setBoards] = useState([]);
-    const [visibleBoardsCount, setVisibleBoardsCount] = useState(7); // Number of boards to show initially
+    // const [newBoardName, setNewBoardName] = useState('');
+    const [boards, setBoards] = useState([
+        // {id: 'staticBackend', name: 'staticBackend'},
+        // {id: 'staticFrontend', name: 'staticFrontend'},
+        // {id: 'staticQa', name: 'staticQA'},
+    ]);
 
-    const { projectName } = useParams();
     const location = useLocation();
 
     useEffect(() => {
         if (location.state) {
-            const { projectId, projectDescription } = location.state;
+            const {projectId, projectDescription} = location.state;
             setProjectId(projectId);
             setProjectDescription(projectDescription);
+
         }
+        // fetchRoles();
     }, [location.state]);
-        console.log(projectDescription);
+
+
+    // const { projectId, projectDescription } = location.state || {};
+
+    console.log('Project ID from workspace:', projectId);
+    console.log('Project Description from workspace:', projectDescription);
+
+
     useEffect(() => {
+        // if (isVisible) {
         fetchRoles();
+
         if (projectId) {
             fetchBoards();
-        }
-    }, [isVisible, projectId]);
 
+        }
+
+        // }
+    }, [isVisible,projectId]);
+
+    // useEffect(() => {
+    //     if (projectId) {
+    //         fetchBoards();
+    //     }
+    // }, [projectId]);
+
+////////////////////////////////////////////////////////Fetches:
     const fetchRoles = async () => {
         try {
             const response = await RoleService.getAllRoles();
+            console.log('API Response:', response);
+
             if (!Array.isArray(response.data)) {
                 throw new Error('Response data is not an array');
             }
+
             const rolesWithIds = response.data.map(role => ({
-                id: role.roleId,
-                roleName: role.roleName
+                // ...role,
+                id: role.roleId,  // Assuming `id` is the correct field from the database
+
+                roleName: role.roleName  // Use roleName as `roleName`
             }));
+
+
             setRoles(rolesWithIds);
         } catch (error) {
             console.error('Error fetching roles:', error);
@@ -52,14 +88,31 @@ const Workspace = ({ isVisible }) => {
     };
 
     const fetchBoards = async () => {
+        let newBoard={ boardId: 'staticBackendID', name: 'staticBackend' }
+        setBoards(prevBoards => [...prevBoards, newBoard]);
+
+        newBoard={ boardId: 'staticFrontendID', name: 'staticFrontend' }
+        setBoards(prevBoards => [...prevBoards, newBoard]);
+
+        newBoard={ boardId: 'staticQaID', name: 'staticQA' }
+        setBoards(prevBoards => [...prevBoards, newBoard]);
         try {
             const response = await BoardService.getBoardsByProject(projectId);
             if (response.data && Array.isArray(response.data)) {
+                console.log('fetchBoards API Response:', response);
+
                 const boardsWithIds = response.data.map(board => ({
-                    boardId: board.boardId,
-                    name: board.name
-                }));
+                    // ...role,
+                        boardId: board.boardId,  // Assuming `id` is the correct field from the database
+
+                    name: board.name  // Use roleName as `roleName`
+                }
+                ));
+
+
                 setBoards(boardsWithIds);
+                console.log('boards array content after fetchBoards:', boards);
+
             } else {
                 throw new Error('Unexpected response format');
             }
@@ -72,58 +125,77 @@ const Workspace = ({ isVisible }) => {
         setSelected_roleId(e.target.value);
     };
 
+
     const handleBoardClick = (boardId) => {
         setSelectedBoard(boardId);
     };
 
     const handleAddClick = () => {
-        setIsDropdownOpen(!isDropdownOpen);
+        setIsDropdownOpen(!isDropdownOpen); // Toggle dropdown visibility
     };
+
+    // const handleAddBoard = () => {
+    //     if (newBoardName.trim() !== '') {
+    //         const newBoardId = newBoardName.toLowerCase().replace(/\s+/g, '-'); // Generate a unique ID for the new board
+    //         setBoards([...boards, { id: newBoardId, name: newBoardName }]); // Add the new board
+    //         setNewBoardName(''); // Clear the input field
+    //         setIsDropdownOpen(false); // Close the dropdown after adding the board
+    //     }
+    // };
+
 
     const handleAddBoard = async () => {
         if (!projectId || !selected_roleId) return;
+
         try {
+            // Fetch the role details by ID
             const response = await RoleService.getRoleById(selected_roleId);
             const role = response.data;
+
             if (!role) {
                 console.error('Role not found for the selected role ID:', selected_roleId);
                 return;
             }
+
             const newBoard = {
-                boardId: selected_roleId,
-                name: role.roleName || 'New Board'
+                // id: selectedRole, // Use the role ID as the board ID
+                // name: roles.find(role => role.id === selectedRole)?.roleName || 'New Board'
+                // name: roles.find(role => role.id === selected_roleId)?.roleName || 'New Board'
+                // name: roles.find().roleName
+                //name: means board name
+                // name: ourRole ? ourRole.roleName : 'New Board'
+                // name: selected_roleId || 'New Board'
+                id: selected_roleId,
+                name: role.roleName || 'New Board',
             };
+
             await BoardService.createBoard(projectId, selected_roleId);
-            setBoards(prevBoards => [...prevBoards, newBoard]);
+            setBoards([...boards, newBoard]);
+
             setSelected_roleId(null);
+
             setIsDropdownOpen(false);
         } catch (error) {
             console.error('Error creating board:', error);
         }
     };
-
-    const handleShowMore = () => {
-        setVisibleBoardsCount(prevCount => prevCount + 5); // Adjust the number of boards to show more
-    };
-
     const handleCloseDropdown = () => {
-        setIsDropdownOpen(false);
+        setIsDropdownOpen(false); // Close the dropdown
+        // onClose(); // Perform any additional cleanup if necessary
     };
 
     const handleDeleteBoard = (boardId) => {
-        setBoards(boards.filter(board => board.boardId !== boardId));
+        setBoards(boards.filter((board) => board.id !== boardId)); // Remove the board
     };
-
-    const visibleBoards = boards.slice(0, visibleBoardsCount);
-    const isShowMoreButtonVisible = boards.length > visibleBoardsCount;
 
     return (
         <div className="layout">
-            <Navbar onLogout={() => { }} />
+            <Navbar onLogout={() => {
+            }}/>
             <nav className="secondary-navbar">
                 <ul className="secondary-nav">
-                    {visibleBoards.map((board) => (
-                        <li key={board.boardId} className="secondary-nav-item">
+                    {boards.map((board) => (
+                        <li key={board.id} className="secondary-nav-item">
                             <div className="board-container">
                                 <Link
                                     className="secnav-link"
@@ -145,38 +217,59 @@ const Workspace = ({ isVisible }) => {
                             </div>
                         </li>
                     ))}
-                    {isShowMoreButtonVisible && (
-                        <button onClick={handleShowMore} className="show-more-button">Show More</button>
-                    )}
+
+
+
                     <button onClick={handleAddClick} className="add-board-button">+</button>
                     {isDropdownOpen && (
-                        <div className={`dropdownaddboard ${isVisible ? 'visible' : ''}`}>
+                        <div className={`
+                        dropdownaddboard ${isVisible ? 'visible' : ''}
+                        `}>
+
+
+                            {/*<input*/}
+                            {/*    type="text"*/}
+                            {/*    value={newBoardName}*/}
+                            {/*    onChange={(e) => setNewBoardName(e.target.value)}*/}
+                            {/*    placeholder="Enter board name"*/}
+                            {/*/>*/}
+                            {/*<button onClick={handleAddBoard}>Add</button>*/}
+
+                            {/*<div className={`modal ${isVisible ? 'visible' : ''}`}>*/}
                             <p>Add Board</p>
                             <select value={selected_roleId} onChange={handleRoleChange}>
+
                                 <option value="">Select Role</option>
+
                                 {roles.map(role => (
+
                                     <option key={role.id} value={role.id}>
                                         {role.roleName}
                                     </option>
+
                                 ))}
+
                             </select>
                             <button onClick={handleAddBoard}>Add</button>
                             <button onClick={handleCloseDropdown}>Cancel</button>
+                            {/*</div>*/}
+
                         </div>
                     )}
                 </ul>
             </nav>
             <div className="main-content">
                 <Routes>
-                    <Route path="/:boardId/:name" element={<Boards />} />
+                    <Route path="/:boardId/:name" element={<Boards/>}/>
                 </Routes>
             </div>
         </div>
     );
 };
-
+// Add PropTypes validation
 Workspace.propTypes = {
     isVisible: PropTypes.bool.isRequired,
+    // onClose: PropTypes.func.isRequired,
+    // onAddBoard: PropTypes.func.isRequired,
 };
-
 export default Workspace;
