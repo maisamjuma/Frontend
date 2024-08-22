70c4620f7cbf73d8f3222258295c97184dbcecb4
OsaidB <100098496+OsaidB@users.noreply.github.com>
2024-08-22 12:48:58 +0300

    the last commit before integrating spring security

--- src/components/_auth/register/index.jsx
+++ src/components/_auth/register/index.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 import {Navigate, Link, useNavigate} from 'react-router-dom'
 import {useAuth} from '../../../contexts/authContext'
-import {doCreateUserWithEmailAndPassword} from '../../../firebase/auth'
+import {doCreateUserWithEmailAndPassword} from '../../../Services/authService.js'
 
 const Register = () => {
 
