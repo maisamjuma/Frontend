5a1066372c0478f85623d0686d6e2b4144459242
OsaidB <100098496+OsaidB@users.noreply.github.com>
2024-08-31 12:38:36 +0300

    styling of Employee Directory

--- src/components/ListUser/ListUser.jsx
+++ src/components/ListUser/ListUser.jsx
@@ -71,7 +71,7 @@ const ListUser = () => {
                             <div className="d-flex justify-content-between align-items-center mt-3">
                                 <button
                                     type='button'
-                                    className='btn btn-info'
+                                    className='btn-update-user'
                                     onClick={() => updateUser(user.userId)}
                                 >
                                     Update
